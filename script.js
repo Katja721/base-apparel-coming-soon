1aad350b5864737d1889a1109ef3023608971547
Katja721 <katja.debevc72@gmail.com>
2022-02-22 14:30:12 +0100

    responsive design

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
-let mail_format =
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
 const submitButton = document.querySelector(".submit-button");
 const errorImage = document.querySelector(".hidden");
 const errorParagraph = document.querySelector(".errorParagraph");
+let mail_format =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
 let results = [];
 submitButton.addEventListener("click", function () {
   const emailInput = String(
@@ -17,7 +17,7 @@ submitButton.addEventListener("click", function () {
   } else if (!emailInput.match(mail_format) || "") {
     errorImage.classList.remove("hidden");
     errorParagraph.classList.remove("hidden");
-    document.querySelector(".emailInput").style.border = "solid 2px red";
+    document.querySelector(".emailInput").style.border =
+      "solid 2px hsl(0, 93%, 68%)";
   }
 });
-let checkFirst = /[a-z]^[$@]/;
