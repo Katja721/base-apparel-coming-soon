5dc5979bd20914706b8ed0655da2092940bfd638
Katja721 <katja.debevc72@gmail.com>
2022-04-05 19:43:00 +0200

    mobile one responsive design

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const submitButton = document.querySelector(".submit-button");
 const errorImage = document.querySelector(".hidden");
 const errorParagraph = document.querySelector(".errorParagraph");
+const emailInputField = document.querySelector(".emailInput");
+
+//media queries
+const mediaMobileOne = window.matchMedia("(max-width:400px)");
 let mail_format =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
 let results = [];
@@ -19,5 +23,8 @@ submitButton.addEventListener("click", function () {
     errorParagraph.classList.remove("hidden");
     document.querySelector(".emailInput").style.border =
       "solid 2px hsl(0, 93%, 68%)";
+  }
+  if (mediaMobileOne.matches) {
+    emailInputField.style.margin = "0rem 2.5rem 0rem 2.5rem";
   }
 });
