212c2b656a57f0a65aa2fec17e157514906564f3
Katja721 <katja.debevc72@gmail.com>
2022-04-08 14:56:05 +0200

    small changes on responsive design

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const emailInputField = document.querySelector(".emailInput");
 
 //media queries
 const mediaMobileOne = window.matchMedia("(max-width:400px)");
+const mediTablet =
+  window.matchMedia("(max-width:999px)") &&
+  window.matchMedia("(min-width:501px)");
+const mediaMobileTwo =
+  window.matchMedia("(max-width:500px)") &&
+  window.matchMedia("(min-width:401px)");
+const mediaLaptop =
+  window.matchMedia("(max-width:1500px)") &&
+  window.matchMedia("(min-width:1000px)");
 let mail_format =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
 let results = [];
@@ -25,6 +34,22 @@ submitButton.addEventListener("click", function () {
       "solid 2px hsl(0, 93%, 68%)";
   }
   if (mediaMobileOne.matches) {
-    emailInputField.style.margin = "0rem 2.5rem 0rem 2.5rem";
+    submitButton.style.left = "-4rem";
+    console.log("mobile 1");
+  }
+  if (mediTablet.matches) {
+    submitButton.style.left = "-14.5rem";
+    console.log("tablet");
+  }
+  if (mediaMobileTwo.matches) {
+    submitButton.style.left = "-13.7rem";
+    console.log("mobile two");
+  }
+  if (mediaLaptop.matches) {
+    submitButton.style.left = "-4rem";
+    console.log("laptop");
   }
 });
+console.log(window.outerWidth);
+console.log(mediTablet.matches === window.innerWidth);
+console.log(mediaLaptop.matches === window.innerWidth);
